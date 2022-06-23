2516447aaf3246213bf84ca138de511f2372b113
touma-morino <touma_064810@yahoo.co.jp>
2022-06-24 01:05:29 +0900

    add: ヘッダーの追加

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,25 @@
 import React from "react";
+import "./Header.css";
+import { useInView } from "react-intersection-observer";
 
 const Header = () => {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <nav>
+        <ul>
+          <li>
+            <a href="#about">ABOUT</a>
+          </li>
+          <li>
+            <a href="skill">SKILL</a>
+          </li>
+          <li>
+            <a href="portfolio">PORTFOLIO</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./Main.css";
 
 const Main = () => {
-  return <div>Main</div>;
+  return <div className="main">Main</div>;
 };
 
 export default Main;
