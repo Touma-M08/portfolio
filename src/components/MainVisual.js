d6902e80625cbe333998434aec5e87d2927a02aa
touma-morino <touma_064810@yahoo.co.jp>
2022-06-23 20:45:55 +0900

    add: メインビジュアルを作成

---
+++ src/components/MainVisual.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./MainVisual.css";
+
+const MainVisual = () => {
+  return (
+    <div className="main-visual">
+      <h1 className="main-ttl">
+        <span className="upper">Toma Morino's</span>
+        <span className="lower">Portfolio</span>
+      </h1>
+    </div>
+  );
+};
+
+export default MainVisual;
